7236743fc95b68264e407df38f9dc43b6b0906e8
TMRomain <romainover51@gmail.com>
2022-02-02 16:36:44 -0500

    Ajout du DAO qui va lister la liste de Technologie

---
+++ app/TechnologiesDAO.js
@@ -0,0 +1,27 @@
+﻿class TechnologiesDAO{
+    constructor(){
+      this.URL = 'http://192.168.56.2/serveur/'
+    }
+  
+    lister(action){
+      fetch(this.URL + 'lister.php')
+        .then(response => response.json())
+        .then(data =>
+          {
+            console.log(data);
+            let listeTech = [];
+            for(let position in data){
+              let technologie = new Technologie(data[position].titre,
+                                      data[position].description,
+                                      data[position].creation,
+                                      data[position].performance,
+                                      data[position].support,
+                                      data[position].id);
+  
+              console.log(technologie);
+              //listeCadeau.push(cadeau);
+            }
+            //action(listeCadeau);
+          });
+    }
+}
