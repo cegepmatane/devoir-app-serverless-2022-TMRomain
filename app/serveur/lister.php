a134188b25c1ea231009392a7cf627e0937ad082
TMRomain <romainover51@gmail.com>
2022-02-02 16:26:35 -0500

    Ajout d'une vue et d'un model ainsi que la fonction qui liste.

---
+++ app/serveur/lister.php
@@ -0,0 +1,14 @@
+﻿<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+header('Content-Type: application/json; charset=utf-8');
+
+$listeTechnologieJson = file_get_contents("liste-technologie.json");
+
+if(strlen($listeTechnologieJson) > 0){
+  $listeTechnologie = json_decode($listeTechnologieJson);
+  echo json_encode($listeTechnologie);
+}else{
+  echo json_encode([]);
+}
---
+++ app/technologie.js
@@ -0,0 +1,11 @@
+﻿class Technologie{
+    constructor(titre,description,creation,performance,support,id){
+      this.titre = titre;
+      this.description = description;
+      this.creation = creation;
+      this.performance = performance;
+      this.support = support;
+      this.id = id;
+    }
+  
+  }
