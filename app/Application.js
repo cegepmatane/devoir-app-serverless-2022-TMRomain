80de3ad7d4fb35c655b3d8738435fbbd9edb524d
TMRomain <romainover51@gmail.com>
2022-02-02 16:44:34 -0500

    Ajout de la logique de la vue principale + logique de navigation.

---
+++ app/Application.js
@@ -0,0 +1,40 @@
+class Application {
+    constructor(window, vuePrincipale,technologiesDAO){
+  
+      this.window = window;
+
+
+      this.vuePrincipale = vuePrincipale;
+  
+      this.technologiesDAO = technologiesDAO;
+  
+      // C'est l'équivalent de function(){this.naviguer()}
+      this.window.addEventListener("hashchange", () =>this.naviguer());
+  
+      this.naviguer();
+    }
+  
+    naviguer(){
+      let hash = window.location.hash;
+  
+      if(!hash){
+        //On est dans la vue principale 
+        this.vuePrincipale.initialiserListeTech(this.technologiesDAO.lister());
+        this.vuePrincipale.afficher();
+  
+      }else{
+  
+        // let navigation = hash.match(/^#jeu\/([0-9]+)/);
+        // let idJeu = navigation[1];
+  
+        // let jeu = this.jeuDAO.chercher(parseInt(idJeu))
+        // this.vueJeu.initialiserJeu(jeu);
+        // this.vueJeu.afficher();
+  
+      }
+    }
+  
+  }
+  
+  new Application(window, new vuePrincipale(),new TechnologiesDAO());
+  
---
+++ app/VuePrincipale.js
@@ -0,0 +1,30 @@
+class vuePrincipale{
+    constructor(){
+      this.html = document.getElementById("html-vue-principale").innerHTML;
+      this.listeTechDonnee = null;
+    }
+  
+    initialiserListeJeu(listeTechDonnee){
+      this.listeTechDonnee = listeTechDonnee;
+    }
+  
+    afficher(){
+      document.getElementsByTagName("body")[0].innerHTML = this.html;
+  
+      let listeTech = document.getElementById("liste-tech");
+      const listeTechItemHTML = listeTech.innerHTML;
+      let listeTechHTMLRemplacement = "";
+  
+      for(var numeroTech in this.listeTechDonnee){
+        let listeTechHTMLRemplacement = listeTechItemHTML;
+        listeTechHTMLRemplacement = listeTechHTMLRemplacement.replace("{Tech.id}",this.listeTechDonnee[numeroTech].id);
+        listeTechHTMLRemplacement = listeTechHTMLRemplacement.replace("{Tech.titre}",this.listeTechDonnee[numeroTech].titre);
+        listeTechHTMLRemplacement = listeTechHTMLRemplacement.replace("{Tech.desc}",this.listeTechDonnee[numeroTech].description);
+        // listeTechHTMLRemplacement = listeTechHTMLRemplacement.replace("{Jeu.note}",this.listeJeuDonnee[numeroJeu].note);
+        listeTechHTMLRemplacement += listeTechHTMLRemplacement;
+      }
+  
+      listeTech.innerHTML = listeTechHTMLRemplacement;
+    }
+  
+  }
